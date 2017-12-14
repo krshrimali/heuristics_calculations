c40b6cd23461a6115394ddb91a80a0531a4e5e19
kushashwa ravi shrimali <kushashwaravishrimali@gmail.com>
2017-12-14 20:09:45 +0530

    Edited sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -1,38 +1,31 @@
-var numberOfRows; //determine the number of rows we want
-var numberOfColumns; //determine the number of columns we want
+var numberOfRows;
+var numberOfColumns; 
 
-var xStep; //determine the size of the gap between two points on the x axis
-var yStep; //determine the size of the gap between two points on the y axis
+var xStep;
+var yStep; 
 
-var positions = []; //an array of positions where we will store each of our Vectors
+var positions = []; 
 
 function setup(){
   createCanvas(500, 500);
   console.log(width);
   console.log(height);
-  numberOfColumns = 16; //we want 16 columns
-  numberOfRows = 16; //we want 9 rows
+  numberOfColumns = 16; 
+  numberOfRows = 16; 
   
-  xStep = width/numberOfColumns; //to make sure they are evenly spaced, we divide the width and height by numberOfColumns
-  yStep = height/numberOfRows; //and numberOfRows respectively
+  xStep = width/numberOfColumns; 
+  yStep = height/numberOfRows; 
   
-  for(var x = 0; x <= width; x += xStep){ //start at the first column, where x = 0
-  
-    for(var y = 0; y <= height; y += yStep){ //go through all the rows (y = 0, y = yStep * 1, y  = yStep * 2, etc.)
-    
-      var p = createVector(x, y); //we create a vector at this location
-      
-      positions.push(p); // and then we put the vector into the array
-      
+  for(var x = 0; x <= width; x += xStep){
+    for(var y = 0; y <= height; y += yStep){
+      var p = createVector(x, y);      
+      positions.push(p);
     }
-    //at the end of the inner for loop, we go back to the first loop, and we increment x
-    //now our column is going to be x = xStep*1, and we populate all the rows with the inner for loop
-    //and again, and again until x > width
   }
 }
 
 function draw(){
-  fill(250, 100, 100); //pink is cool
+  fill(250, 100, 100); 
   
   for(var i = 0; i < positions.length; i++){ //go through all our positions
     line(positions[i].x, positions[i].y, positions[i].x + height, positions[i].y); //put a circle at each of them
