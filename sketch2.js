2d1001ebc5d3ad6870e5ad379d5bd06989d59820
kushashwa ravi shrimali <kushashwaravishrimali@gmail.com>
2017-12-15 09:11:15 +0530

    Added SLD/Euclidean Distance in sketch2.js

--- sketch2.js
+++ sketch2.js
@@ -5,7 +5,21 @@ var xStep;
 var yStep; 
 
 var positions = []; 
-
+var items = [];
+function Euclidean(){
+  console.log("YOu are typing: ", this.value());
+  console.log("Items is: ", items);
+  if(items.length >= 2){
+    var item1 = items.pop();
+    var item2 = items.pop();
+    console.log(item1, item2);
+    line(item1.x, item1.y, item2.x, item2.y);
+    // stroke(100);
+  }
+  // line(Ax, Bx, Ay, By);
+  // distance = sqrt(pow((Bx - Ax),2) + pow((By - Ay),2));
+  // console.log("Eucliedian distance is: ", distance);
+}
 function setup(){
   canvas = createCanvas(500, 500);
   console.log(width);
@@ -24,20 +38,24 @@ function setup(){
       positions.push(p);
     }
   }
+  var inp = createInput('Enter two values: ');
+  inp.input(Euclidean);
 } 
 
 function draw(){
-  console.log(positions);
+  // console.log(positions);
   fill(250, 100, 100); 
   // alphabet =  1;
   for(var i = 0; i < positions.length; i++){ //go through all our positions
     line(positions[i].x, positions[i].y, positions[i].x + height, positions[i].y); //put a circle at each of them
     line(positions[i].x, positions[i].y, positions[i].x, positions[i].y + width);
     ellipse(positions[i].x, positions[i].y, 3, 3);
+    // text(positions[i].z, positions[i].x, positions[i].y);
     // text(alphabet, positions[i].x, positions[i].y + 3);
     // alphabet += 1;
   }
   line(500, 0, 500, 500);
+
   // canvas.mouseOver(showElement);
 }
 
@@ -50,21 +68,26 @@ function mousePressed(){
   var flagA = 0;
   var flagB = 0;
   console.log("Mouse pressed");
+  console.log(mouseX, mouseY);
+  console.log("xStep is: ", xStep);
+  console.log("yStep is: ", yStep);
   for(var i = 0; i < positions.length; i++){
-    if(mouseX % positions[i].x < xStep){
+    if(abs(mouseX - positions[i].x) < xStep && flagA == 0 && abs(mouseY - positions[i].y) < yStep && flagB == 0){
       var A = positions[i].x;
       flagA = 1;
-      console.log(flagA, A);
-    }
-    if(mouseY % positions[i].y < yStep){
+      console.log("flagA, A", flagA, A);
       var B = positions[i].y;
-      flagB = 1;
+      flagB = 1;      // console.log("")
       var index = i;
+      var p = createVector(A, B);
+      items.push(p);
+      // console.log("Items is: ", items);
+      console.log("Index is: ", i);
+      text(positions[index].z, A, B);
     }
-    
   }
   // if(mouseX in positions){
   //   console.log("Yes!")
   // }
-  text(positions[index].z, A, B);
+  // text(positions[index].z, A, B);
 }
